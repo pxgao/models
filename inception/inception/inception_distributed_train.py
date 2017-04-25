4a0ffa40a59cd56aff808d4de84a18d1ce770848
EC2 Default User <ec2-user@ip-10-0-0-42.us-west-2.compute.internal>
2017-04-25 18:53:21 +0000

    calculating loss

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -174,8 +174,8 @@ def train(target, dataset, cluster_spec):
       # global statistics.
       # This is not needed when the number of replicas are small but important
       # for synchronous distributed training with tens of workers/replicas.
-      #exp_moving_averager = tf.train.ExponentialMovingAverage(
-      #    inception.MOVING_AVERAGE_DECAY, global_step)
+      exp_moving_averager = tf.train.ExponentialMovingAverage(
+          inception.MOVING_AVERAGE_DECAY, global_step)
 
       #variables_to_average = (tf.trainable_variables() + tf.moving_average_variables())
 
@@ -215,11 +215,11 @@ def train(target, dataset, cluster_spec):
 
       apply_gradients_op = opt.apply_gradients(grads, global_step=global_step)
 
-      #with tf.control_dependencies([apply_gradients_op]):
-      #  train_op = tf.identity(apply_gradients_op, name='train_op')
-      #Ensures that all the gradients are applied before total loss is calculated
       with tf.control_dependencies([apply_gradients_op]):
-        train_op = tf.identity(total_loss, name='train_op')
+        train_op = tf.identity(apply_gradients_op, name='train_op')
+      #Ensures that all the gradients are applied before total loss is calculated
+      #with tf.control_dependencies([apply_gradients_op]):
+      #  train_op = tf.identity(total_loss, name='train_op')
 
       # Get chief queue_runners, init_tokens and clean_up_op, which is used to
       # synchronize replicas.
