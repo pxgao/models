82c0a6be57b6eb89c9e6bad94fa7b89ab663dba0
michael chang <michaelc@berkeley.edu>
2017-05-02 11:15:52 -0700

    only communciate image

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -93,17 +93,10 @@ def train(target, dataset, cluster_spec):
   # hosts string.
   num_workers = len(cluster_spec.as_dict()['worker'])
   num_parameter_servers = len(cluster_spec.as_dict()['ps'])
-  # If no value is given, num_replicas_to_aggregate defaults to be the number of
-  # workers.
   if FLAGS.num_replicas_to_aggregate == -1:
     num_replicas_to_aggregate = num_workers
   else:
     num_replicas_to_aggregate = FLAGS.num_replicas_to_aggregate
-
-  # Both should be greater than 0 in a distributed training.
-  assert num_workers > 0 and num_parameter_servers > 0, (' num_workers and '
-                                                         'num_parameter_servers'
-                                                         ' must be > 0.')
 
   # Choose worker 0 as the chief. Note that any worker could be the chief
   # but there should be only one chief.
@@ -122,105 +115,13 @@ def train(target, dataset, cluster_spec):
       # Calculate the learning rate schedule.
       num_batches_per_epoch = (dataset.num_examples_per_epoch() /
                                FLAGS.batch_size)
-      # Decay steps need to be divided by the number of replicas to aggregate.
-      decay_steps = int(num_batches_per_epoch * FLAGS.num_epochs_per_decay /
-                        num_replicas_to_aggregate)
-
-      # Decay the learning rate exponentially based on the number of steps.
-      lr = tf.train.exponential_decay(FLAGS.initial_learning_rate,
-                                      global_step,
-                                      decay_steps,
-                                      FLAGS.learning_rate_decay_factor,
-                                      staircase=True)
-      # Add a summary to track the learning rate.
-      tf.scalar_summary('learning_rate', lr)
-
-      # Create an optimizer that performs gradient descent.
-      opt = tf.train.RMSPropOptimizer(lr,
-                                      RMSPROP_DECAY,
-                                      momentum=RMSPROP_MOMENTUM,
-                                      epsilon=RMSPROP_EPSILON)
-
+      
       images, labels = image_processing.distorted_inputs(
           dataset,
           batch_size=FLAGS.batch_size,
           num_preprocess_threads=FLAGS.num_preprocess_threads)
 
-      # Number of classes in the Dataset label set plus 1.
-      # Label 0 is reserved for an (unused) background class.
-      num_classes = dataset.num_classes() + 1
-      logits = inception.inference(images, num_classes, for_training=True)
-      # Add classification loss.
-      inception.loss(logits, labels)
-
-      # Gather all of the losses including regularization losses.
-      losses = tf.get_collection(slim.losses.LOSSES_COLLECTION)
-      losses += tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-
-      total_loss = tf.add_n(losses, name='total_loss')
-
-      if is_chief:
-        # Compute the moving average of all individual losses and the
-        # total loss.
-        loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
-        loss_averages_op = loss_averages.apply(losses + [total_loss])
-
-        # Attach a scalar summmary to all individual losses and the total loss;
-        # do the same for the averaged version of the losses.
-        for l in losses + [total_loss]:
-          loss_name = l.op.name
-          # Name each loss as '(raw)' and name the moving average version of the
-          # loss as the original loss name.
-          tf.scalar_summary(loss_name + ' (raw)', l)
-          tf.scalar_summary(loss_name, loss_averages.average(l))
-
-        # Add dependency to compute loss_averages.
-        with tf.control_dependencies([loss_averages_op]):
-          total_loss = tf.identity(total_loss)
-
-      # Track the moving averages of all trainable variables.
-      # Note that we maintain a 'double-average' of the BatchNormalization
-      # global statistics.
-      # This is not needed when the number of replicas are small but important
-      # for synchronous distributed training with tens of workers/replicas.
-      exp_moving_averager = tf.train.ExponentialMovingAverage(
-          inception.MOVING_AVERAGE_DECAY, global_step)
-
-      variables_to_average = (
-          tf.trainable_variables() + tf.moving_average_variables())
-
-      # Add histograms for model variables.
-      for var in variables_to_average:
-        tf.histogram_summary(var.op.name, var)
-
-      # Create synchronous replica optimizer.
-      opt = tf.train.SyncReplicasOptimizer(
-          opt,
-          replicas_to_aggregate=num_replicas_to_aggregate,
-          replica_id=FLAGS.task_id,
-          total_num_replicas=num_workers,
-          variable_averages=exp_moving_averager,
-          variables_to_average=variables_to_average)
-
-      batchnorm_updates = tf.get_collection(slim.ops.UPDATE_OPS_COLLECTION)
-      assert batchnorm_updates, 'Batchnorm updates are missing'
-      batchnorm_updates_op = tf.group(*batchnorm_updates)
-      # Add dependency to compute batchnorm_updates.
-      with tf.control_dependencies([batchnorm_updates_op]):
-        total_loss = tf.identity(total_loss)
-
-      # Compute gradients with respect to the loss.
-      grads = opt.compute_gradients(total_loss)
-
-      # Add histograms for gradients.
-      for grad, var in grads:
-        if grad is not None:
-          tf.histogram_summary(var.op.name + '/gradients', grad)
-
-      apply_gradients_op = opt.apply_gradients(grads, global_step=global_step)
-
-      with tf.control_dependencies([apply_gradients_op]):
-        train_op = tf.identity(total_loss, name='train_op')
+      multiply_op = tf.mul(images, 2.0)
 
       # Get chief queue_runners, init_tokens and clean_up_op, which is used to
       # synchronize replicas.
@@ -228,12 +129,6 @@ def train(target, dataset, cluster_spec):
       chief_queue_runners = [opt.get_chief_queue_runner()]
       init_tokens_op = opt.get_init_tokens_op()
       clean_up_op = opt.get_clean_up_op()
-
-      # Create a saver.
-      saver = tf.train.Saver()
-
-      # Build the summary operation based on the TF collection of Summaries.
-      summary_op = tf.merge_all_summaries()
 
       # Build an initialization operation to run below.
       init_op = tf.initialize_all_variables()
@@ -276,29 +171,15 @@ def train(target, dataset, cluster_spec):
       while not sv.should_stop():
         try:
           start_time = time.time()
-          loss_value, step = sess.run([train_op, global_step])
-          assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-          if step > FLAGS.max_steps:
-            break
+          result = sess.run(multiply_op)
           duration = time.time() - start_time
 
-          if step % 30 == 0:
-            examples_per_sec = FLAGS.batch_size / float(duration)
-            format_str = ('Worker %d: %s: step %d, loss = %.2f'
-                          '(%.1f examples/sec; %.3f  sec/batch)')
-            tf.logging.info(format_str %
-                            (FLAGS.task_id, datetime.now(), step, loss_value,
-                             examples_per_sec, duration))
-
-          # Determine if the summary_op should be run on the chief worker.
-          if is_chief and next_summary_time < time.time():
-            tf.logging.info('Running Summary operation on the chief.')
-            summary_str = sess.run(summary_op)
-            sv.summary_computed(sess, summary_str)
-            tf.logging.info('Finished running Summary operation.')
-
-            # Determine the next time for running the summary.
-            next_summary_time += FLAGS.save_summaries_secs
+          examples_per_sec = FLAGS.batch_size / float(duration)
+          format_str = ('Worker %d: %s: step %d, loss = %.2f'
+                        '(%.1f examples/sec; %.3f  sec/batch)')
+          tf.logging.info(format_str %
+                          (FLAGS.task_id, datetime.now(), step, loss_value,
+                           examples_per_sec, duration))
         except:
           if is_chief:
             tf.logging.info('About to execute sync_clean_up_op!')
--- inception/inception/inception_train.py
+++ inception/inception/inception_train.py
@@ -187,26 +187,6 @@ def train(dataset):
         'global_step', [],
         initializer=tf.constant_initializer(0), trainable=False)
 
-    # Calculate the learning rate schedule.
-    num_batches_per_epoch = (dataset.num_examples_per_epoch() /
-                             FLAGS.batch_size)
-    decay_steps = int(num_batches_per_epoch * FLAGS.num_epochs_per_decay)
-
-    # Decay the learning rate exponentially based on the number of steps.
-    lr = tf.train.exponential_decay(FLAGS.initial_learning_rate,
-                                    global_step,
-                                    decay_steps,
-                                    FLAGS.learning_rate_decay_factor,
-                                    staircase=True)
-
-    # Create an optimizer that performs gradient descent.
-    opt = tf.train.RMSPropOptimizer(lr, RMSPROP_DECAY,
-                                    momentum=RMSPROP_MOMENTUM,
-                                    epsilon=RMSPROP_EPSILON)
-
-    # Get images and labels for ImageNet and split the batch across GPUs.
-    assert FLAGS.batch_size % FLAGS.num_gpus == 0, (
-        'Batch size must be divisible by number of GPUs')
     split_batch_size = int(FLAGS.batch_size / FLAGS.num_gpus)
 
     # Override the number of preprocessing threads to account for the increased
@@ -215,8 +195,6 @@ def train(dataset):
     images, labels = image_processing.distorted_inputs(
         dataset,
         num_preprocess_threads=num_preprocess_threads)
-
-    input_summaries = copy.copy(tf.get_collection(tf.GraphKeys.SUMMARIES))
 
     # Number of classes in the Dataset label set plus 1.
     # Label 0 is reserved for an (unused) background class.
@@ -229,6 +207,7 @@ def train(dataset):
     # Calculate the gradients for each model tower.
     tower_grads = []
     reuse_variables = None
+    all_assign_ops = []
     for i in xrange(FLAGS.num_gpus):
       with tf.device('/gpu:%d' % i):
         with tf.name_scope('%s_%d' % (inception.TOWER_NAME, i)) as scope:
@@ -237,74 +216,11 @@ def train(dataset):
             # Calculate the loss for one tower of the ImageNet model. This
             # function constructs the entire ImageNet model but shares the
             # variables across all towers.
-            loss = _tower_loss(images_splits[i], labels_splits[i], num_classes,
-                               scope, reuse_variables)
-
-          # Reuse variables for the next tower.
-          reuse_variables = True
-
-          # Retain the summaries from the final tower.
-          summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
-
-          # Retain the Batch Normalization updates operations only from the
-          # final tower. Ideally, we should grab the updates from all towers
-          # but these stats accumulate extremely fast so we can ignore the
-          # other stats from the other towers without significant detriment.
-          batchnorm_updates = tf.get_collection(slim.ops.UPDATE_OPS_COLLECTION,
-                                                scope)
-
-          # Calculate the gradients for the batch of data on this ImageNet
-          # tower.
-          grads = opt.compute_gradients(loss)
-
-          # Keep track of the gradients across all towers.
-          tower_grads.append(grads)
-
-    # We must calculate the mean of each gradient. Note that this is the
-    # synchronization point across all towers.
-    grads = _average_gradients(tower_grads)
-
-    # Add a summaries for the input processing and global_step.
-    summaries.extend(input_summaries)
-
-    # Add a summary to track the learning rate.
-    summaries.append(tf.scalar_summary('learning_rate', lr))
-
-    # Add histograms for gradients.
-    for grad, var in grads:
-      if grad is not None:
-        summaries.append(
-            tf.histogram_summary(var.op.name + '/gradients', grad))
-
-    # Apply the gradients to adjust the shared variables.
-    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
-
-    # Add histograms for trainable variables.
-    for var in tf.trainable_variables():
-      summaries.append(tf.histogram_summary(var.op.name, var))
-
-    # Track the moving averages of all trainable variables.
-    # Note that we maintain a "double-average" of the BatchNormalization
-    # global statistics. This is more complicated then need be but we employ
-    # this for backward-compatibility with our previous models.
-    variable_averages = tf.train.ExponentialMovingAverage(
-        inception.MOVING_AVERAGE_DECAY, global_step)
-
-    # Another possiblility is to use tf.slim.get_variables().
-    variables_to_average = (tf.trainable_variables() +
-                            tf.moving_average_variables())
-    variables_averages_op = variable_averages.apply(variables_to_average)
-
-    # Group all updates to into a single train op.
-    batchnorm_updates_op = tf.group(*batchnorm_updates)
-    train_op = tf.group(apply_gradient_op, variables_averages_op,
-                        batchnorm_updates_op)
-
-    # Create a saver.
-    saver = tf.train.Saver(tf.all_variables())
-
-    # Build the summary operation from the last tower summaries.
-    summary_op = tf.merge_summary(summaries)
+            image_shape = tf.shape(images_splits[i])
+            params = tf.get_variable("params", shape=image_shape,        
+                             initializer=tf.zeros_initializer)
+            assign_op = tf.assign(params, images_splits[i])
+            all_assign_ops.append(assign_op)
 
     # Build an initialization operation to run below.
     init = tf.initialize_all_variables()
@@ -317,36 +233,14 @@ def train(dataset):
         log_device_placement=FLAGS.log_device_placement))
     sess.run(init)
 
-    if FLAGS.pretrained_model_checkpoint_path:
-      assert tf.gfile.Exists(FLAGS.pretrained_model_checkpoint_path)
-      variables_to_restore = tf.get_collection(
-          slim.variables.VARIABLES_TO_RESTORE)
-      restorer = tf.train.Saver(variables_to_restore)
-      restorer.restore(sess, FLAGS.pretrained_model_checkpoint_path)
-      print('%s: Pre-trained model restored from %s' %
-            (datetime.now(), FLAGS.pretrained_model_checkpoint_path))
-
-    # Start the queue runners.
     tf.train.start_queue_runners(sess=sess)
-    #Allow time for the the queues to fill up with training examples
-    print('LOGGING WARNING: Starting the queue runner')
-    queue_size = sess.run("batch/fifo_queue_Size:0")
-    print('The current queue size is %.3f' % queue_size)
-    time.sleep(120)
-    queue_size = sess.run("batch/fifo_queue_Size:0")
-    print('LOGGING WARNING: Waking up after 60 seconds of filling in the queue')
-    print('The current queue size is %.3f' % queue_size)
-
-    summary_writer = tf.train.SummaryWriter(
-        FLAGS.train_dir,
-        graph_def=sess.graph.as_graph_def(add_shapes=True))
+    #Sleep for 30 seconds to ensure that there is sufficient data loaded
+    time.sleep(30)
 
     for step in xrange(FLAGS.max_steps):
       start_time = time.time()
-      _, loss_value = sess.run([train_op, loss])
+      _, loss_value = sess.run(assign_op)
       duration = time.time() - start_time
-
-      assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
 
       examples_per_sec = FLAGS.batch_size / float(duration)
       format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
@@ -354,11 +248,3 @@ def train(dataset):
       print(format_str % (datetime.now(), step, loss_value,
                             examples_per_sec, duration))
 
-      if step % 100 == 0:
-        summary_str = sess.run(summary_op)
-        summary_writer.add_summary(summary_str, step)
-
-      # Save the model checkpoint periodically.
-      if step % 5000 == 0 or (step + 1) == FLAGS.max_steps:
-        checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
-        saver.save(sess, checkpoint_path, global_step=step)
